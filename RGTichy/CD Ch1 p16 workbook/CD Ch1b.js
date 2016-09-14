13b036ca4e17eeea4a07208681eed6905e65de53
Robert Tichy <robtichy@AOL.com>
2016-09-14 18:39:10 -0400

    p16

--- RGTichy/CD Ch1 p16 workbook/CD Ch1b.js
+++ RGTichy/CD Ch1 p16 workbook/CD Ch1b.js
@@ -1,10 +1,3 @@
-array=[2,3,4,5,6,7];
-
-var myNumber=42;
-var myName='Robert';
-console.log(myNumber,myName)
-var temp=myNumber;
-myNumber=myName;
-myName=temp;
-
-console.log(myNumber,myName)
+for (var i=-52; i <= 1066; i++) {
+  console.log(i);
+}
--- RGTichy/CD Ch1 workbook/CD Ch1b.js
+++
@@ -1,3 +0,0 @@
-for (var i=-52; i <= 1066; i++) {
-  console.log(i);
-}
