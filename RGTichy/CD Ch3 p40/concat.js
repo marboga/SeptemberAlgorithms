a364dbf1b173ff5d6b1343295ee1cdf390cb54a2
Robert Tichy <robtichy@AOL.com>
2016-09-15 08:17:31 -0400

    Ch.3, pt.2 c

--- RGTichy/CD Ch3 p40/concat.js
+++ RGTichy/CD Ch3 p40/concat.js
@@ -1,6 +1,9 @@
 function concat(arr1,arr2,result) {
   console.log ("result="+result)
-  result = arr1;
+  //result = arr1;
+  for (var i=0; i<arr1.length; i++) {
+    result[i]=arr1[i];
+  }
   console.log ("arr1="+arr1)
   console.log ("arr2="+arr2)
   console.log ("result="+result)
@@ -18,4 +21,5 @@ var r = ['x','z'];
 console.log ("Before " + array1+" and "+array2)
 console.log("concat("+array1+" and "+array2+")")
 console.log (concat(array1,array2,r))
+console.log("array1:"+array1)
 console.log (r)
