983ca04f50312fd96b5d29b80cddb8ef024bfb63
Robert Tichy <robtichy@AOL.com>
2016-09-23 10:40:25 -0400

    9/24/2016 thru commonSuffix()

--- RGTichy/CD Ch3 p40/arrayRotate.js
+++ RGTichy/CD Ch3 p40/arrayRotate.js
@@ -1,4 +1,3 @@
-var array= [1,"A",3,4,5,6];
 function pushFront(arr,value){
   for (var i = arr.length-1; i >= 0; i--) {
     arr[i+1]=arr[i];
@@ -6,7 +5,7 @@ function pushFront(arr,value){
   arr[0]=value;
   return arr;
 }
-function arrayReverse(arr,shiftBy) {
+function rotateArray(arr,shiftBy) {
   if (shiftBy/1 == shiftBy) {
 var tmp = 0;
   for (var i=1; i <= shiftBy; i++) {
@@ -20,5 +19,5 @@ else {
 }
 }
 var array= [1,"A",3,4,5,6];
-console.log (arrayReverse(array,3))
+console.log (rotateArray(array,3))
 console.log(array)
