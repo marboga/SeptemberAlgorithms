810691ce2c4da532787e7f26bb69c30b8cc46a72
salarsen <larsen.spencer@gmail.com>
2016-10-02 11:14:29 -0700

    array day 3 complete

--- SpencerLarsen/arraysday3.js
+++ SpencerLarsen/arraysday3.js
@@ -26,17 +26,40 @@
 //     }
 // }
 
-//array filter range
-// console.log(filterRange([1,2,3,4,5],2,4));
+//array filter range (does not maintain order)
+//console.log(filterRange([1,2,3,4,5],2,4));
+// console.log(filterRange([2,5,3,1,0,6,3,8,4,9],2,4));
 // function filterRange(arr,min,max){
-//     var temp = 0;
-        // for(var i = 0; i < arr.length; i++){
-        //     if(arr[i] < min || arr[i] > max){
-        //         temp++;
-        //     }
-        //     arr[i] = arr[i + 1]
-        // }
-        // arr.length = arr.length - temp;
+//     var index = 0;
+//     while(index < arr.length){
+//         if(arr[index] < min || arr[index] > max){
+//             arr[index] = arr[arr.length - 1];
+//             arr.pop();
+//         } else {
+//             index++;
+//         }
+//         console.log(arr);
+//     }
+//     return arr;
+// }
+
+//array filter - maintain order
+// console.log(filterRange([1,2,3,4,5],2,4));
+// console.log(filterRange([2,5,3,1,0,6,3,8,4,9],2,4));
+// function filterRange(arr,min,max){
+//     var index = 0;
+//     while(index < arr.length){
+//         if(arr[index] < min || arr[index] > max){ //lets shift everything down and pop the last
+//             for (var i = index; i < arr.length - 1; i++){
+//                 arr[i] = arr[i+1];
+//
+//                 //console.log(arr);
+//             }
+//             arr.pop();
+//         } else {
+//             index++;
+//         }
+//     }
 //     return arr;
 // }
 
